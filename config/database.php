21b6362b248d259e9defa6993c85bc774e36edbb
GoTechs <GoTechs>
2019-08-12 15:54:25 -0500

    config database to deploy

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DATABASE_URL',' mysql://b4b8764f30a2bf:72302bfa@us-cdbr-iron-east-02.cleardb.net/heroku_1374d6000e3a962?reconnect=true'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-02.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_1374d6000e3a962'),
+            'username' => env('DB_USERNAME', 'b4b8764f30a2bf'),
+            'password' => env('DB_PASSWORD', '72302bfa'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
