95c46aa81d75e8e18c1dae80adcdcbe1834f2c3b
GoTechs <GoTechs>
2019-08-12 14:31:32 -0500

    deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-//use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-      //  URL::forceScheme('https');
+        URL::forceScheme('https');
     }
 }
