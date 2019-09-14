31cc080b2f4ece461b24dd9f70e61c3295f7c964
GoTechs <GoTechs>
2019-09-14 07:31:36 -0500

    add middleware('auth') to check if user is logged in

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('/archives','ProfilController@archives')->middleware('auth');
 
 // Dynamic dropDown Categorie And Sous categorie
 
-Route::get('/addAd','categorieController@categories');
+Route::get('/addAd','categorieController@categories')->middleware('auth');
 
 Route::get('/json-sousCategorie','categorieController@sousCat')->middleware('auth');
 
