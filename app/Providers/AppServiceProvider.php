b3b92e61e7c85b330c0f5694d4f9776c88776185
GoTechs <GoTechs>
2019-09-22 05:03:15 -0500

    Test forget password

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //URL::forceScheme('https');
-        //Schema::defaultStringLength(191);
+        URL::forceScheme('https');
+        Schema::defaultStringLength(191);
     }
 }
