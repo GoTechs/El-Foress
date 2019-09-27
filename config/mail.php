1c9b73763baf1d05d6eefaee1e9c512b9322bfbb
GoTechs <GoTechs>
2019-09-27 09:44:03 -0500

    update mail file

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'kaddour.slimane@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => 'kaddour.slimane@gmail.com',
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => 'realmadrid31',
 
     /*
     |--------------------------------------------------------------------------
