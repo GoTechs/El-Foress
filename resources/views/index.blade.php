564aba6074f4139a2084e96a383ccd3847bcc5ef
GoTechs <GoTechs>
2019-11-25 19:41:43 +0100

    test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -357,7 +357,7 @@
               </div>
               <div class="desc">
                 <h3 class="counter">
-                 50
+                 100
                 </h3>
                 <p>{{__('index.number_visitors')}}</p>
               </div>
