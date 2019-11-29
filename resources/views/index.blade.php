295c726842bb0a259070f643081ecb41986d4314
GoTechs <GoTechs>
2019-11-29 21:35:15 +0100

    test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -357,7 +357,7 @@
               </div>
               <div class="desc">
                 <h3 class="counter">
-                 23
+                 55
                 </h3>
                 <p>{{__('index.number_visitors')}}</p>
               </div>
