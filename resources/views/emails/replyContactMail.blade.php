d6cc2a8aafd9eb1003506872bf4c72c427433532
GoTechs <GoTechs>
2020-02-19 21:10:38 -0500

    Add auto reply message

---
+++ resources/views/emails/replyContactMail.blade.php
@@ -0,0 +1,19 @@
+@component('mail::message')
+
+
+Bonjour {{$name}},
+
+Votre message a bien été envoyé à notre équipe de support client! Un représentant de Foress répondra à votre demande sous peu.
+
+Si vous souhaitez mettre à jour votre message, vous pouvez le faire en répondant à cet e-mail.
+
+Meilleures salutations,
+
+L'équipe Foress
+
+@endcomponent
+
+
+
+
+
