a0f06db7dd3dc4116f225e44f4f9425d494f5597
GoTechs <GoTechs>
2020-02-21 17:45:35 -0500

    Errors handle

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,25 @@
+
+@extends('layout')
+
+@section('content')
+
+  <!-- Start Content -->
+  <div id="content">
+    <div class="container">        
+      <div class="row">
+        <div class="col-md-12">              
+
+            <div class="alert alert-warning" role="alert">
+                Bon, il semblerait que la page n'existe plus.
+                La page a sûrement été retirée ou le lien URL a une petite faute.
+                <a href='/'><strong> Retour à la page d'accueil</strong></a> 
+            </div>
+
+        </div>      
+      </div>
+    </div>      
+  </div>
+
+  <!-- End Content -->
+
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,24 @@
+
+@extends('layout')
+
+@section('content')
+
+  <!-- Start Content -->
+  <div id="content">
+    <div class="container">        
+      <div class="row">
+        <div class="col-md-12">              
+
+            <div class="alert alert-warning" role="alert">
+                Désolé, Nous ne pouvons pas accéder a cette information pour l'instant. Merci de réessayer plus tard.
+               
+            </div>
+
+        </div>      
+      </div>
+    </div>      
+  </div>
+
+  <!-- End Content -->
+
+@endsection
