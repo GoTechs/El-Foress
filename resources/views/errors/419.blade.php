c33d2e97634897884de79def1abbbf554ff8b896
GoTechs <GoTechs>
2020-02-26 20:30:14 -0500

    Customize 419 page error

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,23 @@
+
+@extends('layout')
+
+@section('content')
+
+  <!-- Start Content -->
+  <div id="content">
+    <div class="container">        
+      <div class="row">
+        <div class="col-md-12">              
+
+            <div class="alert alert-warning" role="alert">
+            Désolé, votre session a expiré. Veuillez actualiser et réessayer.
+            </div>
+
+        </div>      
+      </div>
+    </div>      
+  </div>
+
+  <!-- End Content -->
+
+@endsection
