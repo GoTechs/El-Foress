4ac277fe0c2eb7a3a29bd8d6ccf1e886b8a2a4df
GoTechs <GoTechs>
2020-03-12 14:57:43 -0400

    Custom 403 PAGE

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,23 @@
+
+@extends('layout')
+
+@section('content')
+
+  <!-- Start Content -->
+  <div id="content">
+    <div class="container">        
+      <div class="row">
+        <div class="col-md-12">              
+
+            <div class="alert alert-warning" role="alert">
+                « Accès refusé »               
+            </div>
+
+        </div>      
+      </div>
+    </div>      
+  </div>
+
+  <!-- End Content -->
+
+@endsection
