0435776dacfb3ed1d2fc4c623d043d4b49726adf
slimane.kadour <slimane.kadour@cerence.com>
2020-03-12 19:30:04 -0400

    fix the error width in the desktop

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -8,7 +8,7 @@
     <div class="container">        
       <div class="row">
         <div class="col-md-12">           
-            <div class="alert alert-warning" role="alert">
+            <div class="alert alert-warning col-sm-6 col-sm-offset-4 col-md-4 col-md-offset-4" role="alert">
                Désolé, ce lien a expiré. Veuillez vérifier dans votre boîte de réception que c'est le dernier message que nous avons envoyé.       
             </div>
         </div>      
