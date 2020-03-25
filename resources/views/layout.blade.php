ede8c9d0ec3c62930813ed4be6e5fddfdc0523d4
GoTechs <GoTechs>
2020-03-24 20:46:56 -0400

    Show/hide password field

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -487,7 +487,19 @@ btn.click(function(e)
             $('#form, #contactForm').submit(function() {
                 $('#loading').show();
             });
-            
+
+            function showHidePass(){
+                var passwordInput = document.getElementById('password-field');
+                var passStatus = document.getElementById('pass-status');
+
+                if (passwordInput.type == 'password'){
+                    passwordInput.type='text';
+                    passStatus.className='fa fa-eye-slash field-icon toggle-password';
+                } else {
+                    passwordInput.type='password';
+                    passStatus.className='fa fa-eye field-icon toggle-password';
+                }
+            }
 
   </script>
 
