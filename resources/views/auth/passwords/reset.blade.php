cff26d8c3e953a981a484738ce72b1c0baf65ca7
GoTechs <GoTechs>
2020-04-29 14:27:50 -0400

    Update layout path

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,4 +1,4 @@
-@extends('../../layout')
+@extends('../../profil/layout')
 
     @section('content')
 
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,5 +1,5 @@
 
-    @extends('../layout')
+    @extends('../profil/layout')
 
     @section('content')
 
